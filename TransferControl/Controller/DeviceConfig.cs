064816f67ab7bd4654a21a3b442a99dc7333f3ad
ethan_huang <ethan_huang@sanwa-eng.com.tw>
2018-05-23 18:48:01 +0800

    change log4net version

--- TransferControl/Controller/DeviceConfig.cs
+++ TransferControl/Controller/DeviceConfig.cs
@@ -8,15 +8,15 @@ namespace TransferControl.Controller
 {
     public class DeviceConfig
     {
-        public string DeviceName  { get; set; }
-        public string DeviceType  { get; set; }
-        public string IPAdress  { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceType { get; set; }
+        public string IPAdress { get; set; }
         public int Port { get; set; }
-        public string ConnectionType  { get; set; }
-    public string PortName { get; set; }
-    public int BaudRate { get; set; }
-    public string ParityBit { get; set; }
-    public int DataBits { get; set; }
-    public string StopBit { get; set; }
-  }
+        public string ConnectionType { get; set; }
+        public string PortName { get; set; }
+        public int BaudRate { get; set; }
+        public string ParityBit { get; set; }
+        public int DataBits { get; set; }
+        public string StopBit { get; set; }
+    }
 }
