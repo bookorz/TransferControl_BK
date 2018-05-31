9a29b69f46b1b3d8f6a50844580fe70a2e7eab38
ethan_huang <ethan_huang@sanwa-eng.com.tw>
2018-05-31 18:00:31 +0800

    增加ALARM功能

---
+++ TransferControl/Management/AlarmInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferControl.Management
+{
+    public class AlarmInfo
+    {
+        public string NodeName { get; set; }
+        public string SystemAlarmCode { get; set; }
+        public string AlarmCode { get; set; }
+        public string Desc { get; set; }
+        public string EngDesc { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}
---
+++ TransferControl/Management/AlarmManagement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferControl.Management
+{
+    public class AlarmManagement
+    {
+        private static List<AlarmInfo> AlarmList = new List<AlarmInfo>();
+        private static List<AlarmInfo> AlarmHistory = new List<AlarmInfo>();
+
+        public static void Add(AlarmInfo Alm)
+        {
+            AlarmList.Add(Alm);
+            AlarmHistory.Add(Alm);
+            
+        }
+
+        public static void Clear()
+        {
+            AlarmList.Clear();
+        }
+
+        public static List<AlarmInfo> GetAll()
+        {
+            List<AlarmInfo> result = null;
+            result = AlarmList.ToList();
+            return result;
+        }
+
+        public static List<AlarmInfo> GetHistory()
+        {
+            List<AlarmInfo> result = null;
+            result = AlarmHistory.ToList();
+            return result;
+        }
+
+        public static void Remove(string NodeName)
+        {
+            var find = from Alm in AlarmList.ToList()
+                       where Alm.NodeName.Equals(NodeName)
+                       select Alm;
+
+            if (find.Count() != 0)
+            {
+                foreach(AlarmInfo each in find)
+                {
+                    AlarmList.Remove(each);
+                }
+            }
+            
+            
+        }
+    }
+}
